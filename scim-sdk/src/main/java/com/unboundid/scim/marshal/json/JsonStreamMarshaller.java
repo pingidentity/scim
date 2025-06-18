f46c60ffc250e49dac8079b8a2e0d36a06b9ba97
kolehey <88401154+kolehey@users.noreply.github.com>
2025-06-18 17:34:37 -0500

    Update copyright years to 2025

--- scim-sdk/src/main/java/com/unboundid/scim/marshal/json/JsonStreamMarshaller.java
+++ scim-sdk/src/main/java/com/unboundid/scim/marshal/json/JsonStreamMarshaller.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2024 Ping Identity Corporation
+ * Copyright 2012-2025 Ping Identity Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License (GPLv2 only)
@@ -16,7 +16,7 @@
  */
 
 /*
- * Copyright 2011-2024 Ping Identity Corporation
+ * Copyright 2011-2025 Ping Identity Corporation
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License (GPLv2 only)
